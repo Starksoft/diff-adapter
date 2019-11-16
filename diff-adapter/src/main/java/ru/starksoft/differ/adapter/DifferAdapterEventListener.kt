31e98e62d3c551452b21abfd96f6bf1820cffe5c
Anisonyan Edward <4238662+Starksoft@users.noreply.github.com>
2019-11-16 23:46:11 +0300

    1.0.3

--- diff-adapter/src/main/java/ru/starksoft/differ/adapter/DifferAdapterEventListener.kt
+++ diff-adapter/src/main/java/ru/starksoft/differ/adapter/DifferAdapterEventListener.kt
@@ -4,6 +4,17 @@ import androidx.recyclerview.widget.ListUpdateCallback
 import ru.starksoft.differ.adapter.viewmodel.ViewModel
 
 abstract class DifferAdapterEventListener : ListUpdateCallback {
+	override fun onChanged(position: Int, count: Int, payload: Any?) {
+	}
+
+	override fun onMoved(fromPosition: Int, toPosition: Int) {
+	}
+
+	override fun onInserted(position: Int, count: Int) {
+	}
+
+	override fun onRemoved(position: Int, count: Int) {
+	}
 
 	abstract fun onFinished(viewModelList: List<ViewModel>)
 }
